fedab365d5fb9f6f3da4a28acf96b0d696d1bb65
ilya <123ilya@gmail.com>
2023-07-24 21:31:49 +1000

    scissors bag

--- rock_paper_scissors/app.js
+++ rock_paper_scissors/app.js
@@ -6,6 +6,7 @@ const resultDisplay = document.getElementById("result"); //Получаем до
 const possibleChoices = document.querySelectorAll("button");
 let userChoice;
 let computerChoice;
+let result;
 //Пробегаемся по массиву possibleChoices и для каждого элемента масива выполняем следующее:
 possibleChoices.forEach((possibleChoice) =>
   //к каждому элементу массива(html элементу <button>) устанавливаем обработчик события. В данном случае обработчик
@@ -19,6 +20,7 @@ possibleChoices.forEach((possibleChoice) =>
     //Изменяем содержимое элемента, записаннног в переменную userChoiceDisplay на id элемента на котором произошло событие.
     userChoiceDisplay.innerHTML = userChoice;
     generateComputerChoice();
+    getResult();
   })
 );
 function generateComputerChoice() {
@@ -34,3 +36,29 @@ function generateComputerChoice() {
   }
   computerChoiceDisplay.innerHTML = computerChoice;
 }
+function getResult() {
+  if (computerChoice === userChoice) {
+    result = "it is draw!";
+  }
+  if (computerChoice === "rock" && userChoice === "paper") {
+    result = "you win!";
+  }
+  if (computerChoice === "rock" && userChoice === "scisors") {
+    result = "you lost!";
+  }
+  if (computerChoice === "paper" && userChoice === "scisors") {
+    result = "you win!";
+  }
+  if (computerChoice === "paper" && userChoice === "rock") {
+    result = "you lost!";
+  }
+  if (computerChoice === "scisors" && userChoice === "rock") {
+    result = "you win!";
+  }
+  if (computerChoice === "scisors" && userChoice === "paper") {
+    result = "you lost!";
+  }
+  resultDisplay.innerHTML = result;
+  console.log(result);
+}
+//проблема. когда пользователь выбирает scisors, то результат не отображается
