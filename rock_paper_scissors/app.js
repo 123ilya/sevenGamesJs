9d0de7b688397d290d22840ccefb65672e5369f0
ilya <123ilya@gmail.com>
2023-07-24 21:06:40 +1000

    computer choice randomly changes agree with user choice

--- rock_paper_scissors/app.js
+++ rock_paper_scissors/app.js
@@ -5,6 +5,7 @@ const resultDisplay = document.getElementById("result"); //Получаем до
 //Получаем массив содержащий все элементы <button> и записываем его в переменную possibleChoices
 const possibleChoices = document.querySelectorAll("button");
 let userChoice;
+let computerChoice;
 //Пробегаемся по массиву possibleChoices и для каждого элемента масива выполняем следующее:
 possibleChoices.forEach((possibleChoice) =>
   //к каждому элементу массива(html элементу <button>) устанавливаем обработчик события. В данном случае обработчик
@@ -17,5 +18,19 @@ possibleChoices.forEach((possibleChoice) =>
     userChoice = e.target.id;
     //Изменяем содержимое элемента, записаннног в переменную userChoiceDisplay на id элемента на котором произошло событие.
     userChoiceDisplay.innerHTML = userChoice;
+    generateComputerChoice();
   })
 );
+function generateComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * possibleChoices.length);
+  if (randomNumber === 0) {
+    computerChoice = "rock";
+  }
+  if (randomNumber === 1) {
+    computerChoice = "scisors";
+  }
+  if (randomNumber === 2) {
+    computerChoice = "paper";
+  }
+  computerChoiceDisplay.innerHTML = computerChoice;
+}
